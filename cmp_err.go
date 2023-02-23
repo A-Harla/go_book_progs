4f4527c9a0b5d1c0b9b8f67563d2d1f4dc9f8426
A-Harla <87483257+A-Harla@users.noreply.github.com>
2023-02-24 00:00:30 +0300

    Add files via upload

--- cmp_err.go
+++ cmp_err.go
@@ -6,19 +6,42 @@ package main
 import (
 	"errors"
 	"io"
+	"log"
 	"os"
 	"strconv"
 )
+
+var LOGFILE = "/tmp/mGo.log"
+
+func catchIoError(err error) {
+	f, err := os.OpenFile(LOGFILE, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+	// Здесь мы создаем новый журнальный файл, используя функцию os.OpenFile() с необходимыми правами доступа к UNIX-файлам (0644).
+
+	iLog := log.New(f, "customLogLineNumber ", log.LstdFlags)
+	iLog.SetFlags(log.LstdFlags)
+	iLog.Println("Error while trying to use io.WriteString!")
+	iLog.Println(err.Error())
+	// функция SetFlags позволяет устанавливать выходные флаги (варианты) для текущего средства журналирования.
+	//По умолчанию функция предлагает значения LstdFlags: Ldate и Ltime, то есть в каждой записи журнала,
+	//которая записывается в журнальный файл, будут указаны текущая дата и время.
+}
 
 func main() {
 	arguments := os.Args // считываем количество переданных функции аргументов
 	// Возможная ошибка 1 - неверное количество переданных аргументов
 	if len(arguments) < 2 { // 1 арг - имя программы, 2 и 3 - числа для сравнения
 		err := errors.New("You should give at least 2 arguments!\n")
-		io.WriteString(os.Stderr, err.Error()) // выводим сообщени об ошибке в консоль
-		io.WriteString(os.Stdout, "\n")
+		_, errIo := io.WriteString(os.Stderr, err.Error()) // выводим сообщени об ошибке в консоль
+		if errIo != nil {
+			catchIoError(errIo)
+			os.Exit(10)
+		}
+		_, errIo = io.WriteString(os.Stdout, "\n")
+		if errIo != nil {
+			catchIoError(errIo)
+			os.Exit(10)
+		}
 	}
-	// io.WriteString также возвращает одним из аргументов ошибку, но мы не будем её отлавливать, в целях экономии времени
 
 	// Возможная ошибка 2 - аргументы не числа
 	arg1, err := strconv.ParseFloat(arguments[1], 32)
@@ -26,30 +49,65 @@ func main() {
 		arg2, err := strconv.ParseFloat(arguments[2], 32)
 		if err == nil { // если второе тоже преобразовалось, то сравниваем их
 			if arg1 == arg2 { // при равенстве аргументов выводим в консоль сообщение об этом
-				io.WriteString(os.Stdout, "Arguments are equal!\n")
+				_, errIo := io.WriteString(os.Stdout, "Arguments are equal!\n")
+				if errIo != nil {
+					catchIoError(errIo)
+					os.Exit(10)
+				}
 				return
 			} else {
 				if arg1 < arg2 { // В случае если аргументы не равны, определяем какой из аргументов больше
 					result := "Arguments are not equal!\n Min: " + arguments[2] + "\n Max: " + arguments[1] + "\n"
-					io.WriteString(os.Stdout, result)
+					_, errIo := io.WriteString(os.Stdout, result)
+					if errIo != nil {
+						catchIoError(errIo)
+						os.Exit(10)
+					}
 					return
 				} else {
 					result := "Arguments are not equal!\n Min: " + arguments[1] + "\n Max: " + arguments[2] + "\n"
-					io.WriteString(os.Stdout, result)
+					_, errIo := io.WriteString(os.Stdout, result)
+					if errIo != nil {
+						catchIoError(errIo)
+						os.Exit(10)
+					}
 					return
 				}
 			}
 		} else {
-			io.WriteString(os.Stdout, "Second argument is incorrect!\n")
-			io.WriteString(os.Stderr, err.Error())
-			io.WriteString(os.Stdout, "\n")
+			_, errIo := io.WriteString(os.Stdout, "Second argument is incorrect!\n")
+			if errIo != nil {
+				catchIoError(errIo)
+				os.Exit(10)
+			}
+			_, errIo = io.WriteString(os.Stderr, err.Error())
+			if errIo != nil {
+				catchIoError(errIo)
+				os.Exit(10)
+			}
+			_, errIo = io.WriteString(os.Stdout, "\n")
+			if errIo != nil {
+				catchIoError(errIo)
+				os.Exit(10)
+			}
 			return
 		}
 	} else {
-		io.WriteString(os.Stdout, "First argument is incorrect!\n")
-		io.WriteString(os.Stderr, err.Error())
-		io.WriteString(os.Stdout, "\n")
+		_, errIo := io.WriteString(os.Stdout, "First argument is incorrect!\n")
+		if errIo != nil {
+			catchIoError(errIo)
+			os.Exit(10)
+		}
+		_, errIo = io.WriteString(os.Stderr, err.Error())
+		if errIo != nil {
+			catchIoError(errIo)
+			os.Exit(10)
+		}
+		_, errIo = io.WriteString(os.Stdout, "\n")
+		if errIo != nil {
+			catchIoError(errIo)
+			os.Exit(10)
+		}
 		return
 	}
-
 }
